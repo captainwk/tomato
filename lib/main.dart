a7b806441b6c9532df1dbe2fd5ddbda302eb9db8
black <wkjang@hu1.com>
2022-05-21 19:18:05 +0900

    파일나누기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tomato/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,35 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/golden.jpeg'),
-              Container(height: 100, color: Colors.red),
-              Container(height: 100, color: Colors.blue),
-              Container(height: 100, color: Colors.orange),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
