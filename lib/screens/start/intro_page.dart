f005e69baa0857c0e13646acb727e5f8a56e984b
black <wkjang@hu1.com>
2022-05-23 15:21:13 +0900

    column alignment & padding

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -8,44 +8,51 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const Text(
-            '토마토 마켓',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-            ),
-          ),
-          ExtendedImage.asset('assets/images/carrot_intro.png'),
-          const Text(
-            '우리 동네 중고 직거래 토마토마켓',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Text(
-            '토마토마켓은 동네 직거래 마켓이에요.\n내 동네를 설정하고 시작해보세요!',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          TextButton(
-              onPressed: () {
-                logger.d('on text button clicked!!!');
-              },
-              child: Text(
-                '내 동네 설정하고 시작하기',
-                style: TextStyle(color: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Text(
+              '토마토 마켓',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
               ),
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-              ))
-        ],
+            ),
+            ExtendedImage.asset('assets/images/carrot_intro.png'),
+            const Text(
+              '우리 동네 중고 직거래 토마토마켓',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              '토마토마켓은 동네 직거래 마켓이에요.\n내 동네를 설정하고 시작해보세요!',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  logger.d('on text button clicked!!!');
+                },
+                child: const Text(
+                  '내 동네 설정하고 시작하기',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
